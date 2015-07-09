476bcd154d7d43ba6aab886f607c0fd3a146b206
angeldlm08 <angeldlm08@gmail.com>
2015-07-09 18:08:29 +0300

    added_ansver_to_client

--- Day_4/client.c
+++ Day_4/client.c
@@ -6,24 +6,35 @@
 
 int main()
 {
-char message[] = "Hello World!\n";
-char buf[sizeof(message)];
-int sock;
-struct sockaddr_in addr;
+	char message[] = "Hello World!\n";
+	char buf[sizeof(message)];
+	char buff[1024];
+	int sock, listener;
+	int bytes_read;
+	struct sockaddr_in addr;
+	
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if(sock < 0) {
+		perror("socket");
+		return 1;
+	}
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(3425);
+	addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+		perror("connect");
+		return 1;
+	}
+	send(sock, message, sizeof(message), 0);
 
-sock = socket(AF_INET, SOCK_STREAM, 0);
-if(sock < 0) {
-perror("socket");
-return 1;
+	
+	bytes_read = recv(sock, buff, 1024, 0);
+
+	printf("Receive message: %s\n", buff);
+			
+
+
+	close(sock);
+	return 0;
 }
-addr.sin_family = AF_INET;
-addr.sin_port = htons(3425);
-addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-perror("connect");
-return 1;
-}
-send(sock, message, sizeof(message), 0);
-close(sock);
-return 0;
-}
+
